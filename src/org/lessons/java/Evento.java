af3c95fb39a087e46e8d99ff8baef0e9be14bfaa
Giovanni Ciotta <giociotta@gmail.com>
2023-02-03 12:13:14 +0100

    Milestone 1

--- src/org/lessons/java/Evento.java
+++ src/org/lessons/java/Evento.java
@@ -1,19 +1,52 @@
 package org.lessons.java;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Evento {
 	private String titolo;
-	private LocalDate data = LocalDate.of(2023, 2, 3);
-//	DateTimeFormatter dataf = DateTimeFormatter.ofPattern("dd/MM/yyyy").withLocale(Locale.ITALY);
+	private LocalDate data;
 	private int postiTot, postiPrenotati;
+	private LocalDate dataAttuale = LocalDate.now();
+	private DateTimeFormatter dataf = DateTimeFormatter.ofPattern("dd/MM/yyyy").withLocale(Locale.ITALY);
 
-	public Evento(String titolo, LocalDate data, int postiTot, int postiPrenotati) {
+	public Evento(String titolo, LocalDate data, int postiTot, int postiPrenotati, LocalDate dataAttuale)
+			throws Exception {
 		super();
 		this.titolo = titolo;
 		this.data = data;
+		validazioneData(data);
 		this.postiTot = postiTot;
+		validazionePostiTotali(postiTot);
 		this.postiPrenotati = 0;
+		this.dataAttuale = dataAttuale;
+	}
+
+	public void validazioneData(LocalDate data) throws Exception {
+		if (dataAttuale.isBefore(data))
+			throw new Exception();
+	}
+
+	public void validazionePostiTotali(int postiTot) throws Exception {
+		if (postiTot < 1)
+			throw new Exception();
+	}
+
+	public void prenota(int postiPrenotati) throws Exception {
+		if (dataAttuale.isBefore(data) || (postiTot <= postiPrenotati))
+			throw new Exception();
+		else
+			postiPrenotati++;
+
+	}
+
+	public void disdici(int postiPrenotati) throws Exception {
+		if (dataAttuale.isBefore(data) || (postiTot <= postiPrenotati))
+			throw new Exception();
+		else
+			postiPrenotati--;
+
 	}
 
 	public String getTitolo() {
@@ -25,10 +58,13 @@ public class Evento {
 	}
 
 	public LocalDate getData() {
+
 		return data;
+
 	}
 
 	public void setData(LocalDate data) {
+
 		this.data = data;
 	}
 
@@ -42,8 +78,7 @@ public class Evento {
 
 	@Override
 	public String toString() {
-		return "Evento [titolo=" + titolo + ", data=" + data + ", postiTot=" + postiTot + ", postiPrenotati="
-				+ postiPrenotati + "]";
+		return "Evento [titolo=" + titolo + ", data=" + data.format(dataf) + "]";
 	}
 
 }
